1d6b19b77a666527aae600fca0d1a96ccc08a555
Mick Collins <michael.collins@mulesoft.com>
2020-07-29 10:27:30 -0400

    x

--- mysql-deploy-govcloud-api/govCloudDeploy.sh
+++ mysql-deploy-govcloud-api/govCloudDeploy.sh
@@ -8,16 +8,17 @@ mysqlUsername=$5
 mysqlPassword=$6
 mysqlDatabase=$7
 
-accessToken=$(curl -sk https://gov.anypoint.mulesoft.com/accounts/api/v2/oauth2/token \
--H 'Content-Type: application/json' \
--d "{ 
-	\"client_id\" : \"$client_id\", 
-	\"client_secret\": \"$client_secret\", 
-	\"grant_type\" : \"client_credentials\"
-}" | /usr/local/bin/jq --raw-output .access_token)
 
-echo "Access Token $accessToken"
+#accessToken=$(curl -sk https://gov.anypoint.mulesoft.com/accounts/api/v2/oauth2/token \
+#-H 'Content-Type: application/json' \
+#-d "{ 
+#	\"client_id\" : \"$client_id\", 
+#	\"client_secret\": \"$client_secret\", 
+#	\"grant_type\" : \"client_credentials\"
+#}" | /usr/local/bin/jq --raw-output .access_token)
 
-/usr/local/bin/mvn -f mysql-deploy-govcloud-api/pom.xml clean package deploy -DmuleDeploy -DauthorizationToken=$accessToken -Dmysql.host=$mysqlHost -Dmysql.port=$mysqlPort -Dmysql.username=$mysqlUsername -Dmysql.password=$mysqlPassword -Dmysql.database=$mysqlDatabase -DmuleDeploy
+#echo "Access Token $accessToken"
+
+#/usr/local/bin/mvn -f mysql-deploy-govcloud-api/pom.xml clean package deploy -DmuleDeploy -DauthorizationToken=$accessToken -Dmysql.host=$mysqlHost -Dmysql.port=$mysqlPort -Dmysql.username=$mysqlUsername -Dmysql.password=$mysqlPassword -Dmysql.database=$mysqlDatabase -DmuleDeploy
 
 echo End of Script
