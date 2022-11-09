996185e7c22256ef3228bf3e87ec823765ab4e76
MickCollins-MuleSoft <michael.collins@mulesoft.com>
2022-11-09 15:41:57 -0500

    MMPI ver

--- s-mysql-people-ch-110722-api/chDeploy.sh
+++ s-mysql-people-ch-110722-api/chDeploy.sh
@@ -51,8 +51,8 @@ echo "Access Token $accessToken"
 #
 # ========================================================================
 
-echo 'RUNNING MAVEN: /usr/local/bin/mvn -f s-mysql-people-ch-110722-api/pom.xml clean package deploy -DmuleDeploy -Dmysql.host=' $mysqlHost ' -Dmysql.port=' $mysqlPort ' -Dmysql.user=' $mysqlUser ' -Dmysql.password=' $mysqlPassword ' -Dmysql.database=' $mysqlDatabase
-/usr/local/bin/mvn -f s-mysql-people-ch-110722-api/pom.xml clean package deploy -DmuleDeploy -Dmysql.host=$mysqlHost -Dmysql.port=$mysqlPort -Dmysql.user=$mysqlUser -Dmysql.password=$mysqlPassword -Dmysql.database=$mysqlDatabase -DauthorizationToken=$accessToken 
+echo 'RUNNING MAVEN: /usr/local/bin/mvn -f s-mysql-people-ch-110722-api/pom.xml clean package deploy -DmuleDeploy -Dmysql.host=' $mysqlHost ' -Dmysql.port=' $mysqlPort ' -Dmysql.user=' $mysqlUser ' -Dmysql.password=' $mysqlPassword ' -Dmysql.database=' $mysqlDatabase '-DauthorizationToken='$accessToken 
+/usr/local/bin/mvn -e -f s-mysql-people-ch-110722-api/pom.xml clean package deploy -DmuleDeploy -Dmysql.host=$mysqlHost -Dmysql.port=$mysqlPort -Dmysql.user=$mysqlUser -Dmysql.password=$mysqlPassword -Dmysql.database=$mysqlDatabase -DauthorizationToken=$accessToken 
 
 echo End of Script
 
